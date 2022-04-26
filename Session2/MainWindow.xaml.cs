03a2974c4daf41fff19d2d56875e16a923514503
alexander <82850660+alesaten@users.noreply.github.com>
2022-04-26 12:47:37 +0300

    Add files via upload

---
+++ Session2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EditPriorityButton.Visibility = Visibility.Collapsed;
+            SortBox.SelectedItem = "Без сортировки";
+            FilterBox.SelectedItem = "Все типы";
+        }
+
+        private void SearchBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            SearchBox.Text = "";
+        }
+
+        private void Grid_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            EditPriorityButton.Visibility = Visibility.Visible;
+        }
+    }
+}
